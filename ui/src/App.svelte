ee7d37ea71392e2dd1d3f9175029ca273ccf13a2
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2023-05-31 23:14:12 -0400

    first working demo, no streaming, CSS imperfect

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,25 +1,22 @@
 <script>
-  import { onMount, onDestroy } from 'svelte';
-  import { writable } from 'svelte/store';
-  import { get } from 'svelte/store';
+  import { onMount, onDestroy, afterUpdate } from 'svelte';
+  import { writable, get } from 'svelte/store';
+  // @ts-ignore
   import hljs from 'highlight.js';
+  // @ts-ignore
   import markdownit from 'markdown-it';
 
-  // Variables
-  let chatMessagesDiv;
-  let userInputElem;
-  let systemMessageElem;
-  let modelToggle = false;
-  let modelName = 'gpt-3.5-turbo';
+  let md = new markdownit();
+  let messages = writable([]);
   let systemMessage = '';
   let userInput = '';
-  const messages = writable([]);
   let autoScrollState = true;
-  
-  const md = new markdownit();
+  let modelName = 'gpt-3.5-turbo';
+  let modelToggle = false;
 
-  // Helper functions
-  const renderMarkdown = (content) => md.render(content);
+  function renderMarkdown(content) {
+    return md.render(content);
+  }
 
   function highlightCode(element) {
     const codeElements = element.querySelectorAll("pre code");
@@ -28,13 +25,30 @@
     });
   }
 
-  function autoScroll() {
-    if (autoScrollState) {
-      chatMessagesDiv.scrollTop = chatMessagesDiv.scrollHeight;
+  function updateSystemMessage(systemMessage) {
+    let systemMessageIndex = get(messages).findIndex((message) => message.role === "system");
+    if (systemMessageIndex !== -1) {
+      get(messages).splice(systemMessageIndex, 1);
+    }
+    if (systemMessage) {
+      messages.update(msgs => [...msgs, { role: "system", content: systemMessage }]);
     }
   }
 
-  async function handleResponse(response, messageText) {
+  async function postRequest() {
+    return await fetch("http://localhost:8000/gpt4", {
+      method: "POST",
+      body: JSON.stringify({
+        messages: get(messages),
+        model_type: modelName,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  async function handleResponse(response) {
     const reader = response.body.getReader();
     const decoder = new TextDecoder("utf-8");
     let assistantMessage = "";
@@ -45,79 +59,52 @@
         messages.update(msgs => [...msgs, { role: "assistant", content: assistantMessage }]);
         break;
       }
-
       const text = decoder.decode(value);
       assistantMessage += text;
-      messageText.innerHTML = renderMarkdown(assistantMessage).trim();
-      highlightCode(messageText);
-      autoScroll();
     }
   }
 
-  async function postRequest() {
-    const messagesList = get(messages);
-    return await fetch("/gpt4", {
-      method: "POST",
-      body: JSON.stringify({
-        messages: messagesList,
-        model_type: modelName,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  }
-
-  // Event handlers
-  async function handleFormSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
 
-    userInput = userInputElem.value.trim();
-    systemMessage = systemMessageElem.value.trim();
-
+    updateSystemMessage(systemMessage);
     messages.update(msgs => [...msgs, { role: "user", content: userInput }]);
-    userInputElem.value = "";
-
+    userInput = '';
     const response = await postRequest();
-
-    handleResponse(response, userInput);
+    handleResponse(response);
   }
 
   function handleModelToggle() {
-  if (modelToggle) {
-    modelName = "gpt-4";
-  } else {
-    modelName = "gpt-3.5-turbo";
+    modelToggle = !modelToggle;
+    modelName = modelToggle ? 'gpt-4' : 'gpt-3.5-turbo';
   }
-}
 
+  afterUpdate(() => {
+    const messageDiv = document.querySelectorAll('.message');
+    messageDiv.forEach(div => highlightCode(div));
+  });
 
+  let chatMessagesDiv;
   onMount(() => {
+    chatMessagesDiv = document.getElementById("chat-messages");
     chatMessagesDiv.addEventListener("scroll", function () {
       const isAtBottom =
         chatMessagesDiv.scrollHeight - chatMessagesDiv.clientHeight <=
         chatMessagesDiv.scrollTop + 1;
-  
+
       autoScrollState = isAtBottom;
     });
   });
 
   onDestroy(() => {
-    chatMessagesDiv.removeEventListener('scroll');
+    chatMessagesDiv.removeEventListener("scroll");
   });
-
 </script>
 
-<svelte:head>
-  <title>GPT-4 API Chat</title>
-  <link rel="stylesheet" href="/static/styles.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/tokyo-night-dark.min.css">
-</svelte:head>
-
 <div class="menubar">
-  <input type="text" class="input" bind:this={systemMessageElem} placeholder="Enter system message">
+  <input type="text" class="input" bind:value={systemMessage} placeholder="Enter system message">
   <label class="slider-container">
-    <input type="checkbox" bind:this={modelToggleElem} bind:checked={modelToggle} on:change={handleModelToggle} autocomplete="off">
+    <input type="checkbox" bind:checked={modelToggle} on:change={handleModelToggle}>
     <span class="slider-track">
       <span class="model-label">{modelToggle ? 'GPT-4' : 'GPT-3.5'}</span>
       <div class="slider"></div>
@@ -128,20 +115,15 @@
   <h1 class="heading">Canvas GPT</h1>
   <div class="chat-wrapper">
     <div bind:this={chatMessagesDiv} id="chat-messages" class="chat-box">
-      {#each $messages as message}
+      {#each $messages as message (message.content)}
         <div class={message.role === "user" ? "message user-message" : "message assistant-message"}>
           <p>{@html renderMarkdown(message.content)}</p>
         </div>
       {/each}
     </div>
-    <form id="chat-form" class="input-form" on:submit|preventDefault={handleFormSubmit}>
-      <textarea bind:this={userInputElem} class="input" placeholder="Type your message..."></textarea>
+    <form id="chat-form" class="input-form" on:submit|preventDefault={handleSubmit}>
+      <textarea bind:value={userInput} class="input" placeholder="Type your message..."></textarea>
       <button class="button" type="submit">Send</button>
     </form>
   </div>
 </div>
-
-
-<style>
-
-</style>
